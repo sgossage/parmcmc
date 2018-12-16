915635810c40aca42d646e6613545e66eaae61a5
Seth Gossage <sethg45@gmail.com>
2018-12-15 23:01:01 -0500

    added mistmatch_filters -- converts between mist and MATCH style filter names

---
+++ mistmatch_filters.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def match_to_mist(filters):
+
+    MIST_colors_prefixes = []
+    for i, filtern in enumerate(filters):
+
+        # HST_WFC3 uses the format e.g. WFC3_UVIS_F814W in MIST
+        # This would be UVIS814W in MATCH. So...
+        if "UVIS" in filtern:
+            filters[i] = filtern.replace("UVIS", "WFC3_UVIS_F")
+            MIST_colors_prefixes.append("HST_WFC3")
+
+        # UBVRIplus uses Tycho_B, etc. -- the same 
+        # as MATCH and UBVRIplus is the default 
+        # prefix in MIST_codes, so all good.
+        # (at least for Tycho filters...)
+        elif "Tycho_B" in filtern:
+            MIST_colors_prefixes.append("UBVRIplus")
+
+    return filters, MIST_colors_prefixes
